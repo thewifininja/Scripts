7a4adf85ca616f89f3352b2d39d076611224de65
Nelson Karrick <nkarrick@stepcg.com>
2022-01-28 07:40:21 -0500

    removed useless notess

--- PiTracker/pitracker.py
+++ PiTracker/pitracker.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
-# api-key: ezeyVrHx9VKS3TC6VNbFrGHU1FYUqRp7biATNZk3svL6ApvAmJFJExwrrmQXz7yC3NzGqcboPzGUVzgq
-
 import subprocess
 import requests
 import json
+import sys
+
 
 def rxg_get(path):
 	full_path = base_url + path + "/index.json?api_key=" + api_key
@@ -18,7 +18,6 @@ def rxg_put(path, body):
 
 def rxg_post(path, body):
 	full_path = base_url + path + '/create.json?api_key=' + api_key
-	print("POST")
 	response = requests.post(full_path, data = body, headers = json_headers)
 	return
 
@@ -47,12 +46,16 @@ def create_new_key():
 	response = rxg_post(key_path, new_record)
 	return
 
-ifconfig = subprocess.check_output('ifconfig').decode("utf-8")
+if 'win32' not in sys.platform:
+	ifconfig = subprocess.check_output('ifconfig').decode("utf-8")
+else:
+	ifconfig = subprocess.check_output('ipconfig').decode("utf-8")
+
 hostname = subprocess.check_output('hostname').decode("utf-8")
 json_headers = {'Content-Type':"application/json"}
 
-api_key ='ezeyVrHx9VKS3TC6VNbFrGHU1FYUqRp7biATNZk3svL6ApvAmJFJExwrrmQXz7yC3NzGqcboPzGUVzgq'
-base_url = 'https://rxg.nkarrick.com/admin/scaffolds'
+api_key ='fGV3QNm16XQQfPjmj2fADwh9vABWNy8rWJnWJmsrFnpsFKFCpZ2g78C3yYVqxZfgNeg4JrwKMaqEjNUT'
+base_url = 'https://garage.stepcglabs.com/admin/scaffolds'
 
 existing_key_id = does_current_hostname_exist(hostname)
 
@@ -60,10 +63,3 @@ if existing_key_id:
 	update_existing_key(existing_key_id) 
 else:
 	create_new_key()
-
-
-#curl "https://rxg.nkarrick.com/admin/scaffolds/custom_data_keys/index.json?api_key=ezeyVrHx9VKS3TC6VNbFrGHU1FYUqRp7biATNZk3svL6ApvAmJFJExwrrmQXz7yC3NzGqcboPzGUVzgq"
-# testing
-print()
-print(existing_key_id)
-print()
