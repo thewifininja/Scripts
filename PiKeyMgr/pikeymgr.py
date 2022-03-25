5b884290d26cda23b1a9833ba333593bc7c9a29e
Nelson Karrick <nkarrick@stepcg.com>
2022-03-25 19:52:08 -0400

    Update pikeymgr.py
    Removed uncessary dependencies, and updated to be more modular on the keyfile location and filename

--- PiKeyMgr/pikeymgr.py
+++ PiKeyMgr/pikeymgr.py
@@ -4,7 +4,6 @@ import subprocess
 import requests
 import json
 import sys
-import re
 
 def rxg_get(path):
 	full_path = base_url + path + "/index.json?api_key=" + api_key
@@ -24,19 +23,19 @@ fqdn = sys.argv[1]
 api_key = sys.argv[2]
 base_url = 'https://' + fqdn + '/admin/scaffolds'
 current_keys = []
+keyfile_path = "/home/pi/.ssh/authorized_keys"
 
 current_key_pair_data = get_ssh_keys()
 
 #make a backup of the authorized_keys that exist currently
-subprocess.run('mv /home/pi /home/pi.bak', shell=True)
+subprocess.run('mv ' + keyfile_path + ' ' + keyfile_path + '.bak', shell=True)
 
 for i in current_key_pair_data:
 	current_keys.append(i["public_key"])
 
-keyfile = open("/home/pi/.ssh/authorized_keys", "a")
+keyfile = open(keyfile_path, "a")
 
 for i in current_keys:
-	#subprocess.run("echo " + i + " >> /Users/nkarrick/.ssh/authorized_keys")
 	keyfile.write(i + "\n")
 
 keyfile.close()
