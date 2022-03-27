dbdb4402e544b44011d154d4ad72257b8bbdaeec
Nelson Karrick <nkarrick@stepcg.com>
2022-03-26 20:04:35 -0400

    Update pikeymgr.py
    Updated to allow filtering of Keys by Admin role. Also searching the existing file, and appending, rather than deleting and rewriting every time.

--- PiKeyMgr/pikeymgr.py
+++ PiKeyMgr/pikeymgr.py
@@ -5,6 +5,12 @@ import requests
 import json
 import sys
 
+# Put your keyfile path, and desired admin roles here:
+keyfile_path = "/Users/nkarrick/.ssh/authorized_keys"
+admin_roles_to_import = [
+	"Super User"
+]
+
 def rxg_get(path):
 	full_path = base_url + path + "/index.json?api_key=" + api_key
 	response = json.loads(requests.get(full_path).text)
@@ -12,6 +18,10 @@ def rxg_get(path):
 
 def get_ssh_keys():
 	response = rxg_get('/ssh_keypairs')
+	return response
+
+def get_admin_roles():
+	response = rxg_get('/admin_roles')
 	return response
 
 if len(sys.argv) != 3:
@@ -23,19 +33,27 @@ fqdn = sys.argv[1]
 api_key = sys.argv[2]
 base_url = 'https://' + fqdn + '/admin/scaffolds'
 current_keys = []
-keyfile_path = "/home/pi/.ssh/authorized_keys"
+
+admin_roles = get_admin_roles()
+admin_role_ids = []
+
+for i in admin_roles:
+	if i['name'] in admin_roles_to_import:
+		admin_role_ids.append(i['id'])
 
 current_key_pair_data = get_ssh_keys()
 
-#make a backup of the authorized_keys that exist currently
-subprocess.run('mv ' + keyfile_path + ' ' + keyfile_path + '.bak', shell=True)
-
 for i in current_key_pair_data:
-	current_keys.append(i["public_key"])
+	if i['admin']['admin_role_id'] in admin_role_ids:
+		current_keys.append(i["public_key"])
 
-keyfile = open(keyfile_path, "a")
+keyfile = open(keyfile_path, "a+")
 
 for i in current_keys:
-	keyfile.write(i + "\n")
+	keyfile.seek(0)
+	if (i in keyfile.read()):
+		print("Key Exists")
+	else:
+		keyfile.write("\n" + i)
 
 keyfile.close()
