bb5e00335964b4e91ad203329c9fc8763733896e
Nelson Karrick <nkarrick@stepcg.com>
2023-03-28 16:11:20 -0400

    Updated ips.txt for testing, and fixed graceful exit of multiple processes.

--- nPing/nping.py
+++ nPing/nping.py
@@ -9,7 +9,6 @@ import multiprocessing
 with open('ips.txt', 'r') as file:
     ips = [line.strip() for line in file]
 
-
 def ping(ip, history):
     # Ping the current IP address
     ping_result = ''
@@ -24,6 +23,8 @@ def ping(ip, history):
 
 if __name__ ==  '__main__':
 
+    stop_event = multiprocessing.Event()
+
     ping_history = {}
     manager = multiprocessing.Manager()
 
@@ -32,36 +33,44 @@ if __name__ ==  '__main__':
 
     #ping_history = {ip: multiprocessing.Manager() for ip in ips}  # Initialize a dictionary to store the ping history for each IP
 
-    while True:
-        # Spawn a new process for each IP address in the list
-        processes = [multiprocessing.Process(target=ping, args=(ip,ping_history[ip])) for ip in ips]
+    try:
+        while True:
+            # Spawn a new process for each IP address in the list
+            processes = [multiprocessing.Process(target=ping, args=(ip,ping_history[ip])) for ip in ips]
 
-        # Start all the processes
+            # Start all the processes
+            for process in processes:
+                process.start()
+
+            # Wait for all the processes to finish
+            for process in processes:
+                process.join()
+
+            # Clear the screen
+            os.system('clear')
+
+            print("|==============================================================================================================================|")
+            print("                                                  Welcome to nelliePing v3.0!")
+            print("|==============================================================================================================================|")
+            print("             History: 60 Pings\n")
+
+            for ip in ips:
+                if len(ping_history[ip]) > 60:
+                    ping_history[ip].pop(0)
+                # Print the ping history for the current IP address
+                print(f'\t{ip:<50} [{"".join(["X" if x == 1 else "." for x in ping_history[ip]])}]'.replace(' ', '-'))
+
+            print("\n|==============================================================================================================================|")
+            print("                                                                                                Copyright @TheWiFiNinja 2023")
+            print("|==============================================================================================================================|")
+            print("\n")
+
+            # Wait for 1 second before pinging again
+            #time.sleep(1)
+    except KeyboardInterrupt:
+        stop_event.set()
+
         for process in processes:
-            process.start()
+            process.terminate()
 
-        # Wait for all the processes to finish
-        for process in processes:
-            process.join()
-
-        # Clear the screen
-        os.system('clear')
-
-        print("|==============================================================================================================================|")
-        print("                                                  Welcome to nelliePing v3.0!")
-        print("|==============================================================================================================================|")
-        print("             History: 60 Pings\n")
-
-        for ip in ips:
-            if len(ping_history[ip]) > 60:
-                ping_history[ip].pop(0)
-            # Print the ping history for the current IP address
-            print(f'\t{ip:<50} [{"".join(["X" if x == 1 else "." for x in ping_history[ip]])}]'.replace(' ', '-'))
-
-        print("\n|==============================================================================================================================|")
-        print("                                                                                                Copyright @TheWiFiNinja 2023")
-        print("|==============================================================================================================================|")
-        print("\n")
-
-        # Wait for 1 second before pinging again
-        #time.sleep(1)
+        print("\n\n  Thank you for using nelliePing!\n\n")
